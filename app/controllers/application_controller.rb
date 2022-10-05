5888a04beb27b88a5cc538c3b69cbb434ba261b3
mame0124 <mame0124.rkn@gmail.com>
2022-10-05 20:34:35 +0900

    rubocop、erblint、日本語化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :zipcode, :address, :profile])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :zipcode, :address, :profile])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name zipcode address profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name zipcode address profile])
   end
-
 end
