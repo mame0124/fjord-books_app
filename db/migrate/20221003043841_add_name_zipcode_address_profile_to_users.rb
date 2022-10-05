951f5d322a307f845adb0572a9f98ebd4c6bdd98
mame0124 <mame0124.rkn@gmail.com>
2022-10-05 09:07:58 +0900

    sign_upとedit画面編集、ユーザー一覧と詳細画面追加

---
+++ db/migrate/20221003043841_add_name_zipcode_address_profile_to_users.rb
@@ -0,0 +1,8 @@
+class AddNameZipcodeAddressProfileToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :zipcode, :string
+    add_column :users, :address, :string
+    add_column :users, :profile, :string
+  end
+end
