8d80ecf4ce5acb7c899436044dec46e8d3e20840
mame0124 <mame0124.rkn@gmail.com>
2022-10-12 10:32:15 +0900

    画像登録方法変更、画像ファイルのバリデーション追加,app/views/users/show.html.erbのタイポ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   has_one_attached :image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validate :file_type
+
+  private
+    def file_type
+      if !image.blob.content_type.in?(%('image/jpg image/png image/gif'))
+        errors.add(:image, 'は JPEG 形式または PNG 形式のみ選択してください')
+      end
+    end
 end
