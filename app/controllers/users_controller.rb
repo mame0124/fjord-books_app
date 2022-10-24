46f4ff3f6440cde598bccef02340de59b7364bdb
mame0124 <mame0124.rkn@gmail.com>
2022-10-24 10:08:23 +0900

    users_controller.rbの共通の処理をbefore_actionにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,29 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show following followers]
+  before_action :set_user_name, only: %i[following followers]
+
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def following
-    @user = User.find(params[:id])
-    @user_name = @user.name == '' ? @user.email : @user.name
     @users = @user.following.with_attached_avatar.order(:id).page(params[:page])
   end
 
   def followers
-    @user = User.find(params[:id])
-    @user_name = @user.name == '' ? @user.email : @user.name
     @users = @user.followers.with_attached_avatar.order(:id).page(params[:page])
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def set_user_name
+    @user_name = @user.name == '' ? @user.email : @user.name
   end
 end
