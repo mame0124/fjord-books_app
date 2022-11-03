1021be8c6637c843290e881aeeaf74d058462242
mame0124 <mame0124.rkn@gmail.com>
2022-11-03 18:12:03 +0900

    usersとreportsのモデルテスト追加、システムテスト追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,19 +3,39 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "name or email" do
-    user = User.new(email: 'foo@example.com', name: '')
-    assert_equal 'foo@example.com', user.name_or_email
-    user.name = 'Foo Bar'
-    assert_equal 'Foo Bar', user.name_or_email
+  test 'name_or_email' do
+    user = users(:alice)
+    user.name = 'alice'
+    assert_equal user.name, user.name_or_email
+    user.name = ''
+    assert_equal user.email, user.name_or_email
   end
 
-  test "follow" do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-    assert_not me.following?(she)
-    me.follow(she)
-    assert me.following?(she)
+  test 'follow' do
+    me = users(:alice)
+    other_user = users(:bob)
+    assert_difference 'other_user.followers.count', 1 do
+      me.follow(other_user)
+    end
   end
 
+  test 'unfollow' do
+    carol = users(:carol)
+    dava = users(:dava)
+    assert_difference 'carol.followers.count', -1 do
+      dava.unfollow(carol)
+    end
+  end
+
+  test 'following?' do
+    dava = users(:dava)
+    carol = users(:carol)
+    assert dava.following?(carol)
+  end
+
+  test 'followed_by?' do
+    dava = users(:dava)
+    carol = users(:carol)
+    assert carol.followed_by?(dava)
+  end
 end
